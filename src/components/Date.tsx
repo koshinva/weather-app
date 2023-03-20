b13929d8b64e674b405570c4fa02e5f930242dd1
Koshin Valery <koshinva1993@gmail.com>
2023-03-20 17:17:37 +0300

    feat: отображать текущую дату в компоненте Date

--- src/components/Date.tsx
+++ src/components/Date.tsx
@@ -1,12 +1,14 @@
+import { useTypedSelector } from 'hooks/useTypedSelector';
 import { FC } from 'react'
 
 import styles from './Date.module.scss'
 
 const Date: FC = () => {
+  const {date} = useTypedSelector((store) => store.weather)
   return (
     <div className={styles.date}>
       <h1 className={styles.day}>Today</h1>
-      <p className={styles['week-day']}>Thu, April 28</p>
+      <p className={styles['week-day']}>{date}</p>
     </div>
   );
 }
