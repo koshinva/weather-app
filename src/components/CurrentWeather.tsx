a7da75d8ed62f7043de1493421425ec44e08dc01
Koshin Valery <koshinva1993@gmail.com>
2023-03-21 12:51:44 +0300

    feat: отображение текущих данных погоды в CurrentWeather, рефакторинг

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -1,46 +1,63 @@
-import { FC } from 'react'
-import { FaTint, FaWind, FaCloud } from 'react-icons/fa';
-import {HiSun, HiCloud} from 'react-icons/hi'
+import { FC } from 'react';
+import cn from 'classnames';
 
-import styles from './CurrentWeather.module.scss'
+import { useTypedSelector } from 'hooks/useTypedSelector';
+import { roundNum } from 'utils/roundNum.helpers';
+import BlockWeather from './BlockWeather';
+
+import styles from './CurrentWeather.module.scss';
+import { convertDate } from 'utils/convertDate.helpers';
+
+
 
 const CurrentWeather: FC = () => {
-  
+  const { currentWeather } = useTypedSelector((state) => state.weather);
+  const today = currentWeather?.list[0];
   return (
     <div className={styles.current}>
-      <div className={styles.icons}>
-        <HiSun className={styles.sun} />
-        <HiCloud className={styles['first-cloud']} />
-        <HiCloud className={styles['second-cloud']} />
-        <HiCloud className={styles['third-cloud']} />
+      <div className={cn(styles.details, styles['top-details'])}>
+        <BlockWeather
+          icon="FiSunrise"
+          title="Sunrise"
+          extraStyle="up-level"
+          value={convertDate(currentWeather?.city.sunrise!!, 'H:M')}
+        />
+
+        <img
+          className={styles['image-status']}
+          src={`http://openweathermap.org/img/wn/${today?.weather[0].icon}@2x.png`}
+          alt="icon weather"
+        />
+
+        <BlockWeather
+          icon="FiSunset"
+          title="Sunset"
+          extraStyle="up-level"
+          value={convertDate(currentWeather?.city.sunset!!, 'H:M')}
+        />
       </div>
-      <h2 className={styles.status}>Cloudy</h2>
-      <p className={styles.temperature}>
-        0&#xB0;
+
+      <h2 className={styles.status}>
+        <span>{today?.weather[0].main}</span> ( {today?.weather[0].description} )
+      </h2>
+
+      <p className={styles.temperature}>{roundNum(today?.main.temp || 0)}&#xB0;</p>
+
+      <p className={styles.minmax}>
+        H: {roundNum(today?.main.temp_max || 0, 'ceil')}&#xB0; &nbsp;&nbsp;&nbsp; L:{' '}
+        {roundNum(today?.main.temp_min || 0, 'floor')}
+        &#xB0;
       </p>
+
       <div className={styles.details}>
-        <div className={styles.block}>
-          <FaWind />
-          <p className={styles.index}>
-            0km/h
-          </p>
-          <p className={styles.title}>Wind</p>
-        </div>
-        <div className={styles.block}>
-          <FaCloud />
-          <p className={styles.index}>0%</p>
-          <p className={styles.title}>Cloudiness</p>
-        </div>
-        <div className={styles.block}>
-          <FaTint />
-          <p className={styles.index}>
-            0%
-          </p>
-          <p className={styles.title}>Humidity</p>
-        </div>
+        <BlockWeather icon="FaWind" title="Wind" value={`${roundNum(today?.wind.speed!!)}km/h`} />
+
+        <BlockWeather icon="FaCloud" title="Cloudiness" value={`${today?.clouds.all!!}%`} />
+
+        <BlockWeather icon="FaTint" title="Humidity" value={`${today?.main.humidity!!}%`} />
       </div>
     </div>
   );
-}
+};
 
-export default CurrentWeather
+export default CurrentWeather;
