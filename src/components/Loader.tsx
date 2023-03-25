38b2f5eaa544baf24ccf54d34eb19d7f194cbaf8
Koshin Valery <koshinva1993@gmail.com>
2023-03-25 13:32:54 +0300

    fixed: исправить scss файлы для gh-pages сборки

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 import styles from './Loader.module.scss';
 
-const Loader: FC= () => {
+const Loader: FC = () => {
   return (
     <div className={styles.loader}>
       <div className={styles['loader-body']}>
